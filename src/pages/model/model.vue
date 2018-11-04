eb1ca48e57fd477fa0f0d6adeae4a3a136dbc5c5
zciyi <1434021781@qq.com>
2018-11-04 12:17:07 +0800

    model

--- src/pages/model/model.vue
+++ src/pages/model/model.vue
@@ -25,17 +25,6 @@
               </template>
           </el-table-column>
         </el-table>
-        <div class="pagination">
-          <el-pagination
-          @size-change="handleSizeChange"
-          @current-change="handleCurrentChange"
-          :current-page="currentPage"
-          :page-sizes="pagesSizes"
-          :page-size="pagesSize"
-          layout="total,prev, pager, next,sizes,jumper"
-          :total="total">
-          </el-pagination>
-        </div>
          <el-dialog
           :title="pop.title"
           :visible.sync="pop.visible"
@@ -65,10 +54,6 @@
             var me = this;
             return {
                 tableData: [],
-                currentPage:1,
-                total:0,
-                pagesSize:5,
-                pagesSizes:[5,10,20,30,40,50],
                 form: {
                     theoreticalWeight:"",
                     model:''
@@ -81,7 +66,8 @@
                     },
                     confirm:function(){
                         var validate ={
-                            model:"请输入规格型号"
+                            model:"请输入规格型号",
+                            theoreticalWeight:"请输入理论重量"
                         }
                         var isPass = me.checkData(validate,me.form)
                         if(!isPass||me.load) return
@@ -123,30 +109,19 @@
             }
         },
         created:function(){
-            this.getData(this.currentPage,this.pagesSize);
+            this.getData();
         },
         methods: {
-            getData(pageNo,pageSize){
+            getData(){
                 var me = this;
                 this.$request({
                     url:"/getCarTripModelList",
                     method:"get",
                     query:{
-                    pageNo:pageNo,
-                    pageSize:pageSize
                     }
                 }).then(function(re){
                     me.tableData = re;
-                    me.currentPage = re.pageNo;
-                    me.pagesSize = re.pageSize;
-                    me.total = re.total;
                 })
-            },
-            handleSizeChange(val) {
-                this.getData(this.currentPage,val);
-            },
-            handleCurrentChange(val) {
-                this.getData(val,this.pagesSize);
             },
             checkData(validate,datas){
                 var me =this;
@@ -215,7 +190,7 @@
                     }).then(function(re){
                         me.loadDelete = false;
                         me.tip( '删除成功!');
-                        me.getData(me.currentPage,me.pagesSize);
+                        me.getData();
                     },function(){
                         me.loadDelete = false;
                     })
